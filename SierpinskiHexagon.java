ce2878f4f9fac629ce57dc22ba3c8450a899f151
Justin Farnsworth <farnswj1@tcnj.edu>
2020-09-16 23:18:37 -0400

    fixed typo in comment

--- SierpinskiHexagon.java
+++ SierpinskiHexagon.java
@@ -4,7 +4,7 @@
  * 
  * This program demonstrates recursion by graphing a recursive pattern with hexagons.
  * 
- * Java Source Code:
+ * Java Compile & Run Commands:
  * javac -cp +libs/doodlepad.jar SierpinskiHexagon.java
  * java -cp .;+libs/doodlepad.jar SierpinskiHexagon
  */
